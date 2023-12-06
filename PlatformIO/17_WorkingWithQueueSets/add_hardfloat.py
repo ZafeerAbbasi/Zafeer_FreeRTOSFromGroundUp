6ea9e4caf6bb6000452ca832eee57124e76a05ba
Zafeer Abbasi <zafeerabbasi57@yahoo.com>
2023-12-05 23:41:55 -0800

    idek wtf im doing

--- PlatformIO/17_WorkingWithQueueSets/add_hardfloat.py
+++ PlatformIO/17_WorkingWithQueueSets/add_hardfloat.py
@@ -1,11 +1,17 @@
 Import( "env" )
 
 flags = [
-    "-mfloat-abi=hard",
+    "-mcpu=cortex-m7",
+    "-std=gnu11",
+    "-g3",
+    "-ffunction-sections",
+    "-fdata-sections",
+    "-Wall",
+    "-fstack-usage",
+    "--specs=nano.specs",
     "-mfpu=fpv5-d16",
+    "-mfloat-abi=softfp",
     "-mthumb",
     "-u _printf_float",
-    "-mcpu=cortex-m7",
-    "-DDEBUG -c -x assembler-with-cpp --specs=nano.specs"
 ]
 env.Append(CCFLAGS=flags, LINKFLAGS=flags, ASFLAGS=flags)
